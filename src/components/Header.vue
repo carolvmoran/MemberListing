7b595c7d388fa89d23aee7e8f03d91253c0615f4
carolvmoran <carolvandrade@gmail.com>
2021-03-15 17:15:07 -0400

    Criando Estrutura do Projeto e definindo components

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+.header
+  img(alt="logo", src="../assets/logo.png")
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+.header {
+  background: linear-gradient(to right, #000, #b3d4fc);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  img {
+    padding: 40px;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+.home
+  Header
+  member-listing
+</template>
+
+<script>
+import Header from "@/components/Header";
+import MemberListing from "@/components/MemberListing";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+    MemberListing,
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
